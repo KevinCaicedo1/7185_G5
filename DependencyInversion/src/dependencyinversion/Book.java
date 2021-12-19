876b296fe00fe3dfd5f94a61cfc26f51dfd0e93d
Pablo Andres Cadena Benavides <55595782+pacadena5@users.noreply.github.com>
2021-12-19 18:00:54 -0500

    Add files via upload
    Dependency inversion

---
+++ DependencyInversion/src/dependencyinversion/Book.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dependencyinversion;
+
+/**
+ *
+ * @author G5
+ */
+public class Book implements Product{
+
+    @Override
+    public void getSample() {
+        System.out.println("Im a book");
+    }
+    
+}
