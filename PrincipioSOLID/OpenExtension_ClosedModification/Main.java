4c17fc158806f56d5e6a92048df9a216e0550429
kevincaicedo1 <kocaicedo@espe.edu.ec>
2021-12-19 15:11:41 -0500

    Open for Extension, Closed for Modification Examle

---
+++ PrincipioSOLID/OpenExtension_ClosedModification/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args){
+        SuperCoolGuitarWithFlames guitar = new SuperCoolGuitarWithFlames("Fender", "Stratocaster", 6, "Red");
+        System.out.println(guitar);
+    }
+}
---
+++ PrincipioSOLID/OpenExtension_ClosedModification/SuperCoolGuitarWithFlames.java
@@ -0,0 +1,26 @@
+public class SuperCoolGuitarWithFlames extends Guitar {
+
+    private String flameColor;
+
+    //constructor, getters + setters
+    public SuperCoolGuitarWithFlames(String make, String model, int volume, String flameColor) {
+        super(make, model, volume);
+        this.flameColor = flameColor;
+    }
+
+    public String getFlameColor() {
+        return flameColor;
+    }
+
+    public void setFlameColor(String flameColor) {
+        this.flameColor = flameColor;
+    }
+
+    //toString method
+    @Override
+    public String toString() {
+        return "SuperCoolGuitarWithFlames{" +
+                "flameColor='" + flameColor + '\'' + " is a " +
+                 super.toString() +'}';
+    }
+}
